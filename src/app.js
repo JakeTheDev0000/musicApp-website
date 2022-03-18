7a63dd47e4e84e9710797260ddc6549b16eca2e6
jacob <61372541+JakeTheDev0000@users.noreply.github.com>
2022-03-17 19:21:33 -0700

    added better GUI (THE TIMERS DO NOT WORK)

--- src/app.js
+++ src/app.js
@@ -4,27 +4,43 @@ console.log("started")
 var audio = new Audio('audio_file.mp3');
 audio.play();
 
+THIS IS THE WORST CODE I HAVE EVER WRITTEN, im sorry
 */
 
 isPlayingSound = false
 
-var getLuckyAudio = new Audio('audio/getLucky.mp3');
+var SoundAudio = new Audio('audio/getLucky.mp3');
+
+const musicPlayerTimeCurrent = document.getElementById('musicPlayerTimeCurrent').innerHTML
+const musicPlayerTimeTotal = document.getElementById('musicPlayerTimeTotal').innerHTML
 
 function stopSound(){
     isPlayingSound = false
-    getLuckyAudio.pause()
-    getLuckyAudio.currentTime = 0
+    SoundAudio.pause()
+    SoundAudio.currentTime = 0
 }
-
-function playGetLucky(){
+function pauseSound(){
+    isPlayingSound = false
+    SoundAudio.pause()
+}
+/* GET LUCKY */  
+function playSound(){
     console.log(isPlayingSound)
     if(isPlayingSound){
         return
     }
     isPlayingSound = true
     
-    getLuckyAudio.play();
-    getLuckyAudio.onended = function(){
+    SoundAudio.play();
+    SoundAudio.onended = function(){
         isPlayingSound = false
     }
 }
+
+musicPlayerTimeCurrent.innerHTML = SoundAudio.currentTime
+setInterval(function(){
+    console.log("interval")
+    musicPlayerTimeCurrent.innerHTML = SoundAudio.currentTime
+},1000)
+
+
